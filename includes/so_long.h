d08d46e5943a844485b75758dea29f12231372d7
nelsonmatenda <matendafigueiredo@hotmail.com>
2024-10-01 15:42:58 +0100

    atualiza

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: nfigueir <nfigueir@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:50:21 by nfigueir          #+#    #+#             */
-/*   Updated: 2024/10/01 11:25:13 by nfigueir         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:33:12 by nfigueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,18 @@
 # include <fcntl.h>
 # include <sys/types.h>
 # include <unistd.h>
+
+// ENUM
+enum	e_error
+{
+	MALLOC_ERROR = -20,
+};
+enum	e_game
+{
+	SUCCESS	= 10,
+	LOOSE	= 20,
+	QUIT	= 30
+};
 
 typedef struct s_map
 {
@@ -29,5 +41,6 @@ typedef struct s_game
 	t_map	*map;
 }t_game;
 
+void	*ft_exit(t_game *game, int status, char *msg);
 
 #endif
