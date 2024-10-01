ad07f6b289e18691661004fca298b656abd1913f
nelsonmatenda <matendafigueiredo@hotmail.com>
2024-10-01 11:31:28 +0100

    algumas alterações

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,13 +6,28 @@
 /*   By: nfigueir <nfigueir@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:50:21 by nfigueir          #+#    #+#             */
-/*   Updated: 2024/09/26 14:51:14 by nfigueir         ###   ########.fr       */
+/*   Updated: 2024/10/01 11:25:13 by nfigueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include
+# include "../libft/include/ft_printf.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+typedef struct s_map
+{
+	int	fd;
+}		t_map;
+
+
+typedef struct s_game
+{
+	t_map	*map;
+}t_game;
+
 
 #endif
